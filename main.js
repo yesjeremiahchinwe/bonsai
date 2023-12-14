12d27ed5d17aac2891d09b87dda860c6a45fc388
yesjeremiahchinwe <chinwejeremiah9@gmail.com>
2023-12-14 19:10:44 +0100

    Implemented the navigation feature in the template submenu

--- main.js
+++ main.js
@@ -5,8 +5,11 @@ import "./scss/freelancers.scss"
 import "./scss/pricing.scss"
 import "./scss/coaching.scss"
 import "./scss/collaboration.scss"
+import left_arrow from "./home_page_assets/arrow_left.svg"
+
 
 import { yearlyOffer, monthlyOffer } from "./utils/pricing_offers"
+import { allTemplates } from "./utils/dynamic_templates"
 
 const menuButton = document.querySelector(".menuButton")
 const mobileNav = document.querySelector(".mobile_nav")
@@ -56,6 +59,7 @@ groupOne.addEventListener("mouseover", () => {
 
     headerGroupOne.classList.add("header_group_one")
     productsUl.classList.add("show_products_ul")
+    headerEl.classList.add("add_bg_color")
 })
 
 productsUl.addEventListener("mouseleave", () => {
@@ -78,6 +82,8 @@ groupTwo.addEventListener("mouseover", () => {
 
     headerGroupTwo.classList.add("header_group_two")
     industriesUl.classList.add("show_industries_ul")
+    headerEl.classList.add("add_bg_color")
+
 })
 
 industriesUl.addEventListener("mouseleave", () => {
@@ -100,6 +106,8 @@ groupThree.addEventListener("mouseover", () => {
 
     headerGroupThree.classList.add("header_group_three")
     templatesUl.classList.add("show_templates_ul")
+    headerEl.classList.add("add_bg_color")
+
 })
 
 templatesUl.addEventListener("mouseleave", () => {
@@ -122,6 +130,8 @@ groupFive.addEventListener("mouseover", () => {
 
     headerGroupFour.classList.add("header_group_four")
     resourceUl.classList.add("show_resource_ul")
+    headerEl.classList.add("add_bg_color")
+
 })
 
 resourceUl.addEventListener("mouseleave", () => {
@@ -167,6 +177,7 @@ groupSix.addEventListener("mouseover", () => {
 
 
 
+/* -------------- Add a box-shadow to Header Element on scroll -------------------- */
 document.addEventListener("scroll", () => {
     const scrollY = document.documentElement.scrollTop
 
@@ -180,6 +191,373 @@ document.addEventListener("scroll", () => {
 })
 
 
+
+/* ------------------- Dynamically Render Template Sub-menu ---------------------- */
+const dynamicTemplate = document.querySelector(".dynamic_templates")
+const firstLink = document.querySelector(".first_link")
+const secondLink = document.querySelector(".second_link")
+const thirdLink = document.querySelector(".third_link")
+const fourthLink = document.querySelector(".fourth_link")
+const fivethLink = document.querySelector(".fiveth_link")
+const sixthLink = document.querySelector(".sixth_link")
+const seventhLink = document.querySelector(".seventh_link")
+
+const imgEl = document.createElement("img")
+imgEl.src = `${left_arrow}`
+imgEl.alt = "Right Arrow"
+imgEl.classList.add("temp_left_arrow")
+
+
+let isContactTemp = true
+let isProposalTemp = false
+let isInvoiceTemp = false
+let isQuotesTemp = false
+let isWorkTemp = false
+let isBriefTemp = false
+let isFormTemp = false
+
+
+
+// ------------------------- Contact Template -------------------------
+if (isContactTemp) {
+    dynamicTemplate.innerHTML = allTemplates.contactTemplate()
+    firstLink.classList.add("gray_bg")
+    firstLink.appendChild(imgEl)
+}
+
+
+firstLink.addEventListener("click", () => {
+    isContactTemp = true
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+    if (isContactTemp) {
+        isProposalTemp = false
+        isInvoiceTemp = false
+        isQuotesTemp = false
+        isWorkTemp = false
+        isBriefTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.contactTemplate()
+        firstLink.classList.add("gray_bg")
+        firstLink.appendChild(imgEl)
+    }
+})
+
+
+// ------------------------ Proposal Template -----------------------
+secondLink.addEventListener("click", () => {
+    isProposalTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+    if (isProposalTemp) {
+        isContactTemp = false
+        isInvoiceTemp = false
+        isQuotesTemp = false
+        isWorkTemp = false
+        isBriefTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.proposalTemplate()
+        secondLink.classList.add("gray_bg")
+        secondLink.appendChild(imgEl)
+    }
+})
+
+
+// ------------------- Invoice Template ---------------------
+thirdLink.addEventListener("click", () => {
+    isInvoiceTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+
+    if (isInvoiceTemp) {
+        isProposalTemp = false
+        isContactTemp = false
+        isQuotesTemp = false
+        isWorkTemp = false
+        isBriefTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.invoiceTemplate()
+        thirdLink.classList.add("gray_bg")
+        thirdLink.appendChild(imgEl)
+    }
+})
+
+
+/* ---------------- Quotes Template ----------------- */
+fourthLink.addEventListener("click", () => {
+    isQuotesTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+
+    if (isQuotesTemp) {
+        isProposalTemp = false
+        isContactTemp = false
+        isInvoiceTemp = false
+        isWorkTemp = false
+        isBriefTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.quotesTemplate()
+        fourthLink.classList.add("gray_bg")
+        fourthLink.appendChild(imgEl)
+    }
+})
+
+
+/* ----------------- Work Template ---------------------- */
+fivethLink.addEventListener("click", () => {
+    isWorkTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+
+    if (isWorkTemp) {
+        isProposalTemp = false
+        isContactTemp = false
+        isInvoiceTemp = false
+        isQuotesTemp = false
+        isBriefTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.workTemplate()
+        fivethLink.classList.add("gray_bg")
+        fivethLink.appendChild(imgEl)
+    }
+})
+
+
+/* ------------------- Brief Template ------------------- */
+sixthLink.addEventListener("click", () => {
+    isBriefTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+    if (seventhLink.classList.contains("gray_bg")) {
+        seventhLink.classList.remove("gray_bg")
+        seventhLink.removeChild(imgEl)
+    }
+
+
+    if (isBriefTemp) {
+        isProposalTemp = false
+        isContactTemp = false
+        isInvoiceTemp = false
+        isQuotesTemp = false
+        isWorkTemp = false
+        isFormTemp = false
+        dynamicTemplate.innerHTML = allTemplates.briefTemplate()
+        sixthLink.classList.add("gray_bg")
+        sixthLink.appendChild(imgEl)
+    }
+})
+
+
+/* ----------------- Form Template ----------------- */
+seventhLink.addEventListener("click", () => {
+    isFormTemp = true
+
+    if (firstLink.classList.contains("gray_bg")) {
+        firstLink.classList.remove("gray_bg")
+        firstLink.removeChild(imgEl)
+    }
+
+    if (secondLink.classList.contains("gray_bg")) {
+        secondLink.classList.remove("gray_bg")
+        secondLink.removeChild(imgEl)
+    }
+
+    if (thirdLink.classList.contains("gray_bg")) {
+        thirdLink.classList.remove("gray_bg")
+        thirdLink.removeChild(imgEl)
+    }
+
+    if (fourthLink.classList.contains("gray_bg")) {
+        fourthLink.classList.remove("gray_bg")
+        fourthLink.removeChild(imgEl)
+    }
+    if (fivethLink.classList.contains("gray_bg")) {
+        fivethLink.classList.remove("gray_bg")
+        fivethLink.removeChild(imgEl)
+    }
+    if (sixthLink.classList.contains("gray_bg")) {
+        sixthLink.classList.remove("gray_bg")
+        sixthLink.removeChild(imgEl)
+    }
+
+
+    if (isFormTemp) {
+        isProposalTemp = false
+        isContactTemp = false
+        isInvoiceTemp = false
+        isQuotesTemp = false
+        isWorkTemp = false
+        isBriefTemp = false
+        dynamicTemplate.innerHTML = allTemplates.formTemplate()
+        seventhLink.classList.add("gray_bg")
+        seventhLink.appendChild(imgEl)
+    }
+})
+
+
+
+
+/* ----------------- Toogleing Pricing Offers ---------------------- */
 const offerContainer = document.querySelector(".offer_container")
 const toggleContainer = document.querySelector(".toggle_container")
 const toggleMonthly = document.querySelector(".toggle_monthly")
@@ -193,9 +571,10 @@ if (toggleOffer) {
     toggleYearly.classList.add("background_toggle")
 }
 
+
 toggleContainer.addEventListener("click", () => {
     toggleOffer = !toggleOffer
-    
+
     if (toggleOffer) {
         offerContainer.innerHTML = yearlyOffer()
         toggleMonthly.classList.remove("background_toggle")
@@ -206,4 +585,8 @@ toggleContainer.addEventListener("click", () => {
         toggleMonthly.classList.add("background_toggle")
     }
 })
+
+
+
+
 
---
+++ utils/dynamic_templates.js
@@ -0,0 +1,351 @@
+
+class Templates {
+    constructor() { }
+
+    contactTemplate() {
+        return `
+        <small>Featured contract templates</small>
+        <div class="second_wrapper">
+            <div>
+                <li>
+                    <a href="">Client Contract Template</a>
+                </li>
+                <li>
+                    <a href="">Free General Contract Template</a>
+                </li>
+                <li>
+                    <a href="">Free Independent Contractor Agreement Template</a>
+                </li>
+                <li>
+                    <a href="">Free Online Contract Maker</a>
+                </li>
+                <li>
+                    <a href="">Free Service Agreement Template</a>
+                </li>
+                <li>
+                    <a href="">Graphic Design Contract Template: Free Download</a>
+                </li>
+            </div>
+
+            <div>
+                <li>
+                    <a href="">Free Freelance Contract Template</a>
+                </li>
+                <li>
+                    <a href="">Free Hourly Rate Contract Template</a>
+                </li>
+                <li>
+                    <a href="">Free Non-Disclosure Agreement Template</a>
+                </li>
+                <li>
+                    <a href="">Free Project Management Agreement Template</a>
+                </li>
+                <li>
+                    <a href="">Free Simple Contract Template</a>
+                </li>
+                <li>
+                    <a href="">Project Contract Template</a>
+                </li>
+            </div>
+        </div>
+        `
+    }
+
+    proposalTemplate() {
+
+        return `
+    <small>Featured contract templates</small>
+        <div class="second_wrapper">
+            <div>
+                <li>
+                    <a href="">Free Simple Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Services Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Retainer Proposal Agreement Template</a>
+                </li>
+                <li>
+                    <a href="">Project Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Professional Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Freelance Proposal Template</a>
+                </li>
+            </div>
+
+            <div>
+                <li>
+                    <a href="">Free Freelancer Bid Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Creative Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Contractor Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Collaboration Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Free Bid Proposal Template</a>
+                </li>
+                <li>
+                    <a href="">Accounting Request for Proposal</a>
+                </li>
+            </div>
+        </div>
+    `
+    }
+
+    invoiceTemplate() {
+
+        return `
+<small>Featured contract templates</small>
+    <div class="second_wrapper">
+        <div>
+            <li>
+                <a href="">Free Basic Invoice Template</a>
+            </li>
+            <li>
+                <a href="">Free Blank Invoice Template - Create, Edit & Send</a>
+            </li>
+            <li>
+                <a href="">Free Contractor Invoice Templates</a>
+            </li>
+            <li>
+                <a href="">Free Custom Invoice Template</a>
+            </li>
+            <li>
+                <a href="">Free Invoice Template foe Email</a>
+            </li>
+            <li>
+                <a href="">Free Microsoft Word Invoice Templates</a>
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="">Free Pro Forma Invoice Templates</a>
+            </li>
+            <li>
+                <a href="">Free Retainer Invoice Template</a>
+            </li>
+            <li>
+                <a href="">Free Sample Invoice Template</a>
+            </li>
+            <li>
+                <a href="">Free Work Invoice Template</a>
+            </li>
+            <li>
+                <a href="">Online Invoice Generator</a>
+            </li>
+        </div>
+    </div>
+`
+    }
+
+
+    quotesTemplate() {
+
+        return `
+<small>Featured contract templates</small>
+    <div class="second_wrapper">
+        <div>
+            <li>
+                <a href="">Work Quote Template</a>
+            </li>
+            <li>
+                <a href="">Free Sample Quotation Template</a>
+            </li>
+            <li>
+                <a href="">Simple Quotation Template</a>
+            </li>
+            <li>
+                <a href="">Free Service Quote Template</a>
+            </li>
+            <li>
+                <a href="">Price Quote Template</a>
+            </li>
+            <li>
+                <a href="">Online Quote Template</a>
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="">Free Freelance Quotation Template</a>
+            </li>
+            <li>
+                <a href="">Editable Quotation Template</a>
+            </li>
+            <li>
+                <a href="">Formal Quote Template</a>
+            </li>
+            <li>
+                <a href="">Free Contractor Quote Template</a>
+            </li>
+            <li>
+                <a href="">Creative Quotation Template</a>
+            </li>
+            <li>
+                <a href="">Blank Quote Template</a>
+            </li>
+        </div>
+    </div>
+`
+    }
+
+    workTemplate() {
+
+        return `
+<small>Featured contract templates</small>
+    <div class="second_wrapper">
+        <div>
+            <li>
+                <a href="">Free Website Development Scope of Work Template</a>
+            </li>
+            <li>
+                <a href="">Free Statement of Work Contract Template</a>
+            </li>
+            <li>
+                <a href="">Free Statement of Work (PDF)</a>
+            </li>
+            <li>
+                <a href="">Free SOW for Software Development</a>
+            </li>
+            <li>
+                <a href="">Free Consultant Statement of Work Template</a>
+            </li>
+            <li>
+                <a href="">Remodel Scope Of Work Template</a>
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="">Project Scope Of Work Template</a>
+            </li>
+            <li>
+                <a href="">Free Digital Marketing Scope of Work Template</a>
+            </li>
+            <li>
+                <a href="">Interior Design Scope of Work Template</a>
+            </li>
+            <li>
+                <a href="">Freelance Statement of Work Template</a>
+            </li>
+            <li>
+                <a href="">Contractor Statement of Work Template</a>
+            </li>
+        </div>
+    </div>
+`
+    }
+
+    briefTemplate() {
+
+        return `
+<small>Featured contract templates</small>
+    <div class="second_wrapper">
+        <div>
+            <li>
+                <a href="">Creative Brief (Example)</a>
+            </li>
+            <li>
+                <a href="">Free Brief Template (Word)</a>
+            </li>
+            <li>
+                <a href="">Free Client Brief Template</a>
+            </li>
+            <li>
+                <a href="">Free Concept Brief Template</a>
+            </li>
+            <li>
+                <a href="">Free Consulting Brief</a>
+            </li>
+            <li>
+                <a href="">Free Creative Brief (with Generator)</a>
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="">Free Creative Brief Sample</a>
+            </li>
+            <li>
+                <a href="">Free Creative Brief Template (Word)</a>
+            </li>
+            <li>
+                <a href="">Free Creative Design Brief</a>
+            </li>
+            <li>
+                <a href="">Free Product Brief Template</a>
+            </li>
+            <li>
+                <a href="">Free Project Brief Template</a>
+            </li>
+            <li>
+                <a href="">Free Strategy Brief</a>
+            </li>
+        </div>
+    </div>
+`
+    }
+
+    formTemplate() {
+
+        return `
+<small>Featured contract templates</small>
+    <div class="second_wrapper">
+        <div>
+            <li>
+                <a href="">Website Intake Form</a>
+            </li>
+            <li>
+                <a href="">Project Intake Form</a>
+            </li>
+            <li>
+                <a href="">Free Portfolio Contact Page</a>
+            </li>
+            <li>
+                <a href="">Marketing Client Intake Form</a>
+            </li>
+            <li>
+                <a href="">Comprehensive Client Form</a>
+            </li>
+            <li>
+                <a href="">Client Intake Form</a>
+            </li>
+        </div>
+
+        <div>
+            <li>
+                <a href="">Client Info Form</a>
+            </li>
+            <li>
+                <a href="">Client Feedback Form</a>
+            </li>
+            <li>
+                <a href="">Client Enrollment Form</a>
+            </li>
+            <li>
+                <a href="">Client Contact Form Template</a>
+            </li>
+            <li>
+                <a href="">Business Intake Form</a>
+            </li>
+            <li>
+                <a href="">Business Client Intake Form</a>
+            </li>
+        </div>
+    </div>
+`
+    }
+
+}
+
+export const allTemplates = new Templates()
